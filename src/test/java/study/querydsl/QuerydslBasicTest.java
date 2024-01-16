0f469a09934b8b5fbc89f2b51e4039a44c9c7f17
Khalin94 <mpzx1102@gmail.com>
2024-01-17 01:33:41 +0900

    where, fetch, order,paging

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,7 @@
 package study.querydsl;
 
+import com.querydsl.core.NonUniqueResultException;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,14 +9,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static study.querydsl.entity.QMember.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static study.querydsl.entity.QMember.member;
 
 @Transactional
 @SpringBootTest
@@ -24,6 +27,18 @@ public class QuerydslBasicTest {
     EntityManager em;
 
     JPAQueryFactory queryFactory;
+
+    private Member createMember(String username, int age){
+        return createMember(username, age, null);
+    }
+
+    private Member createMember(String username, int age, Team team){
+        return Member.builder()
+                             .username(username)
+                             .age(age)
+                             .team(team)
+                             .build();
+    }
 
     @BeforeEach
     void before(){
@@ -95,4 +110,127 @@ public class QuerydslBasicTest {
         assertThat(member1.getUsername()).isEqualTo("member1");
 
     }
+
+    @Test
+    public void search() {
+        //given
+        //when
+        Member member1 = queryFactory.selectFrom(member)
+                                     .where(member.age.eq(10)
+                                                      .and(member.username.eq("member1"))
+                                     )
+                                     .fetchOne();
+
+        //then
+        assertThat(member1.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam() {
+        //given
+        //when
+        Member member1 = queryFactory.selectFrom(member)
+                                      .where(
+                                              member.username.eq("member1"),
+                                              member.age.between(10, 30)
+                                      )
+                                      .fetchOne();
+
+        //then
+        assertThat(member1.getUsername()).isEqualTo("member1");
+        assertThat(member1.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    public void fetchTest() {
+        //given
+        //when
+        List<Member> members = queryFactory.selectFrom(member)
+                                           .where(member.username.contains("member"))
+                                           .fetch();
+
+        //then
+        assertThat(members.size()).isEqualTo(4);
+
+        // 리스트가 반환되는 쿼리에 fetchOne을 사용하면 javax.persistence.NonUniqueResultException 이 발생한다.
+        assertThatThrownBy(() ->{
+            queryFactory.selectFrom(member)
+                                         .fetchOne();
+        }).isInstanceOf(NonUniqueResultException.class);
+
+        //fetchFirst = limit 1과 같다
+        Member member1 = queryFactory.selectFrom(member)
+                .orderBy(member.username.asc())
+                                     .fetchFirst();
+        assertThat(member1.getUsername()).isEqualTo("member1");
+
+        QueryResults<Member> results = queryFactory.selectFrom(member)
+                                                              .fetchResults();
+
+        long total = results.getTotal();
+        List<Member> results1 = results.getResults();
+
+        assertThat(total).isEqualTo(4);
+        assertThat(results1.get(0).getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void orderTest() {
+        //given
+        Member member5 = createMember(null, 50);
+        Member member6 = createMember("member6", 50);
+        Member member7 = createMember("member7", 50);
+        em.persist(member5);
+        em.persist(member6);
+        em.persist(member7);
+        //when
+        List<Member> members = queryFactory.selectFrom(member)
+                                         .orderBy(member.age.desc(), member.username.asc()
+                                                                                    .nullsLast()) // null 이면 마지막에 나온다.
+                                         .fetch();
+
+        //then
+        assertThat(members.get(0).getUsername()).isEqualTo("member6");
+        assertThat(members.get(1).getUsername()).isEqualTo("member7");
+        assertThat(members.get(2).getUsername()).isNull();
+
+    }
+
+    @Test
+    public void paging1() {
+        //given
+        //when
+        List<Member> members = queryFactory.selectFrom(member)
+                                         .orderBy(member.username.desc())
+                                         .offset(1)
+                                         .limit(2)
+                                         .fetch();
+
+        for (Member member1 : members) {
+            System.out.println("member1 = " + member1);
+        }
+
+        //then
+        assertThat(members.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2() {
+        //given
+        //when
+        QueryResults<Member> results = queryFactory.selectFrom(member)
+                                                              .orderBy(member.username.desc())
+                                                              .offset(1)
+                                                              .limit(2)
+                                                              .fetchResults();
+
+        //then
+        assertThat(results.getTotal()).isEqualTo(4);
+        assertThat(results.getResults().size()).isEqualTo(2);
+        assertThat(results.getLimit()).isEqualTo(2);
+        assertThat(results.getOffset()).isEqualTo(1);
+    }
+
+
 }
